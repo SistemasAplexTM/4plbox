547b725fd29870f6e098fb66536ac002e08e969b
Duvier <duvierm24@gmail.com>
2019-03-06 08:55:52 -0500

    Impresora con id de usuario

--- app/Http/Controllers/PrintConfigController.php
+++ app/Http/Controllers/PrintConfigController.php
@@ -22,8 +22,7 @@ class PrintConfigController extends Controller
 
     public function save(Request $request)
     {
-      // $key = 'print_'. Auth::user()->agencia_id;
-      $key = 'print_' . php_uname('n');
+      $key = 'print_'. Auth::id();
       $data = array("prints" => $request->data);
       $id = $this->getConfig($key);
       if ($id) {
