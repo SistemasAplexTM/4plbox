c6d7d055f00e95504682c14a1ba5ca7c914d01b9
Duvier <duvier_24@hotmail.es>
2019-08-23 15:01:23 -0500

    desde el portatil

--- resources/assets/js/templates/printConfig.js
+++ resources/assets/js/templates/printConfig.js
@@ -2,7 +2,7 @@
 //   Javascript:jsWebClientPrint.getPrinters();
 // }, 300);
 //var wcppGetPrintersDelay_ms = 0;
-const wcppGetPrintersTimeout_ms = 50000; //5 sec
+const wcppGetPrintersTimeout_ms = 100000; //5 sec
 const wcppGetPrintersTimeoutStep_ms = 500; //0.5 sec
 function wcpGetPrintersOnSuccess() {
   // Display client installed printers
