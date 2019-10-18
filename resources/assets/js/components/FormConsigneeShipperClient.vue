2cdaaefe9d389e11f57277337aa4027bb3fe27d3
Duvier <duvier_24@hotmail.es>
2019-10-18 15:41:55 -0500

    Formato al ese cochino archivo

--- resources/assets/js/components/FormConsigneeShipperClient.vue
+++ resources/assets/js/components/FormConsigneeShipperClient.vue
@@ -322,7 +322,7 @@ export default {
       me.beforeSend();
     });
     bus.$on("update", function(payload) {
-      me.0true);
+      me.true;
     });
     bus.$on("cancel", function(payload) {
       me.resetForm();
